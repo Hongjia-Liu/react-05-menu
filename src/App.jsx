9934700b27f2adc58e5757ce28d0dd4872b736b4
Hongjia Liu <hongjia_liu@outlook.com>
2022-04-11 12:59:14 +0100

    feat: display items

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,23 @@
+import { useState } from "react";
+import items from "./data";
+import Categories from "./Categories";
+import Menu from "./Menu";
+
 function App() {
-	return <h2>Menu Project Setup</h2>;
+	const [menuItems, setMenuItems] = useState(items);
+	const [categories, setCategories] = useState([]);
+	return (
+		<main>
+			<section className="menu section">
+				<div className="title">
+					<h2>our menu</h2>
+					<div className="underline"></div>
+				</div>
+				<Categories />
+				<Menu items={menuItems} />
+			</section>
+		</main>
+	);
 }
 
 export default App;
